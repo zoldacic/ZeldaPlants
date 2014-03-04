73fe8e37dd60a47ae67c40226f971418ce036432
Fredrik Lundgren <k.o.f.lundgren@gmail.com>
2014-03-04 02:51:29 +0100

    Second try with "couldnt open...."

---
+++ ZeldaPlants/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,178 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapGenerator 
+{
+
+    private readonly int _sizeX;
+    private readonly int _sizeY;
+
+    private readonly int _noOfSpriteRows;
+    private readonly int _noOfSpriteColumns;
+
+    private readonly IList<double> _pathProbabilities;
+
+    private enum Direction
+    {
+        North,
+        West,
+        South,
+        East
+    };
+
+    private enum SpriteDirection
+    {
+        Up = 1,
+        Down = -1
+    };
+
+    public int[,] Map { get; private set; }
+
+    public MapGenerator(int sizeX, int sizeY, int noOfSpriteColumns, int noOfSpriteRows, IList<double> pathProbabilities)
+    {
+        _sizeX = sizeX;
+        _sizeY = sizeY;
+
+        _noOfSpriteColumns = noOfSpriteColumns;
+        _noOfSpriteRows = noOfSpriteRows;
+
+        _pathProbabilities = pathProbabilities;
+
+        Map = new int[0, 0];
+    }
+	
+    public void Generate() {
+        Map = new int [_sizeX, _sizeY];
+
+        for (int x = 0; x < _sizeX; x++)
+        {
+            for (var y = 0; y < _sizeY; y++)
+            {
+                Map[x, y] = -1;
+            }
+        }
+
+        var currentX = 0;
+        var currentY = 0;
+        var currentSpriteX = 0;
+        var currentSpriteY = 0;
+        var spriteDirectionColumns = SpriteDirection.Up;
+        var spriteDirectionRows = SpriteDirection.Up;
+
+		Debug.Log ("Retrieving initial possible directions");
+		var possibleDirections = GetPossibleDirections(currentX, currentY);
+
+        while (possibleDirections.Any())
+        {
+            var directions = GetRandomDirections(possibleDirections);
+            AddSpriteNoToMap(ref currentX, ref currentY, ref currentSpriteX, ref currentSpriteY, directions, ref spriteDirectionColumns, ref spriteDirectionRows);
+
+            Debug.Log("Retrieving possible directions");
+            possibleDirections = GetPossibleDirections(currentX, currentY);
+        }
+    }
+
+    private void AddSpriteNoToMap(ref int currentX, ref int currentY, ref int currentSpriteX, ref int currentSpriteY, IEnumerable<Direction> directions, ref SpriteDirection spriteDirectionColumns, ref SpriteDirection spriteDirectionRows)
+    {
+        foreach (var direction in directions)
+        {
+            CheckSpriteDirections(ref currentSpriteX, ref currentSpriteY, ref spriteDirectionColumns, ref spriteDirectionRows, direction);
+
+            switch (direction)
+            {
+                case Direction.West:
+                    currentX -= 1;
+                    break;
+                case Direction.East:
+                    currentX += 1;
+                    break;
+                case Direction.North:
+                    currentY -= 1;
+                    break;
+                case Direction.South:
+                    currentX += 1;
+                    break;
+            }
+
+            Map[currentX, currentY] = currentSpriteY * _noOfSpriteColumns + currentSpriteX;
+        }                
+    }
+
+    private void CheckSpriteDirections(ref int currentSpriteX, ref int currentSpriteY, ref SpriteDirection spriteDirectionColumns, ref SpriteDirection spriteDirectionRows, Direction direction)
+    {
+        if (direction == Direction.West || direction == Direction.East)
+        {
+            var newSpriteX = currentSpriteX + (int) spriteDirectionColumns;
+
+            if (newSpriteX < 0 || newSpriteX > _noOfSpriteColumns - 1)
+            {
+                spriteDirectionColumns = spriteDirectionColumns == SpriteDirection.Up
+                    ? SpriteDirection.Down
+                    : SpriteDirection.Up;
+                newSpriteX = currentSpriteX + (int) spriteDirectionColumns;
+            }
+
+            currentSpriteX = newSpriteX;
+        }
+        else
+        {
+            var newSpriteY = currentSpriteY + (int) spriteDirectionRows;
+
+            if (newSpriteY < 0 || newSpriteY > _noOfSpriteRows - 1)
+            {
+                spriteDirectionColumns = spriteDirectionColumns == SpriteDirection.Up
+                    ? SpriteDirection.Down
+                    : SpriteDirection.Up;
+                newSpriteY = currentSpriteY + (int) spriteDirectionRows;
+            }
+
+            currentSpriteY = newSpriteY;
+        }
+    }
+
+
+    private IEnumerable<Direction> GetRandomDirections(List<Direction> possibleDirections) {
+
+        var directions = new List<Direction>();
+        var pathProbabilityIndex = 0;
+
+        while(possibleDirections.Any()) {
+            var direction = possibleDirections.ToArray()[Random.Range(0, possibleDirections.Count() - 1)];
+
+            var randomNo = Random.Range(0f, 1f);
+            if (randomNo <= _pathProbabilities[pathProbabilityIndex]) {
+                directions.Add(direction);
+
+                if (pathProbabilityIndex < _pathProbabilities.Count() - 1) {
+                    pathProbabilityIndex++;
+                }
+            }
+            
+            possibleDirections.Remove(direction);
+        }
+
+        return directions;
+    }
+
+    private List<Direction> GetPossibleDirections(int currentX, int currentY)
+    {
+        var directions = new List<Direction>();
+
+        CheckDirection(directions, currentX - 1, currentY, Direction.West);
+        CheckDirection(directions, currentX + 1, currentY, Direction.East);
+        CheckDirection(directions, currentX, currentY - 1, Direction.North);
+        CheckDirection(directions, currentX, currentY + 1, Direction.South);
+
+        return directions;
+    }
+
+    private void CheckDirection(ICollection<Direction> directions, int x, int y, Direction direction)
+    {
+        if (x >= 0 && y >= 0 && x < _sizeX && y < _sizeY && Map[x, y] == -1)
+        {
+            directions.Add(direction);
+        }            
+    }
+}
---
+++ ZeldaPlantssx/Assets/Scripts/MapManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public class MapManager : MonoBehaviour
+    {
+		public int SizeX ;
+			public int SizeY;
+			
+        private MapGenerator _mapGenerator;
+
+        void Start ()
+        {
+			Debug.Log("Starting generating");
+            var pathProbablities = new List<double>() {1.0, 0.5};
+	        _mapGenerator = new MapGenerator(SizeX, SizeY, 9, 5, pathProbablities);     
+            _mapGenerator.Generate();
+			var a = 7;
+        }
+	
+        void Update ()
+        {
+
+        }
+
+        void OnGUI()
+        {
+
+
+			var textures = Resources.LoadAll("Desert");
+            var sprites = Resources.LoadAll<Sprite>("Desert");
+			Debug.Log(string.Format("Retrieved {0} textures.", textures.Length));
+			Debug.Log(string.Format("Retrieved {0} sprites.", sprites.Length));
+            //Object[] textures = Resources.LoadAll("Desert");
+            var map = _mapGenerator.Map;
+            for (var x = 0; x < SizeX - 1; x++)
+            {
+                for (var y = 0; y < SizeY - 1; y++)
+                {
+                    //var textureRect = new Rect(x*81, y*98, 81, 98);
+                    //var texture = textures[map[x, y]];
+                    //sprite = Sprite.Create((texture, textureRect, new Vector2(0, 0), 100.0f);
+
+					if (map[x,y] > -1) {
+                        var go = new GameObject();
+                        var spriteRenderer = go.AddComponent<SpriteRenderer>();
+
+						var sprite = sprites[map[x,y]];
+
+                        //go.transform.Translate(x * 80, y * 80, 0);
+                        go.transform.position = new Vector2(x,0); //= new Vector3(x * 80f, y * 80f);
+
+						//sprite.textureRect.x = 0;
+						//sprite.rect.y = 0;
+   
+						spriteRenderer.sprite = sprite;
+						Debug.Log(sprite.rect);
+					}
+                }
+            }
+
+        }
+    }
+}
